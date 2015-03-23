c63e8b98194de97921830f50d3ff585d4a0502d3
Brian Vallelunga <vallelungabrian@gmail.com>
2015-03-23 00:21:57 -0700

    adding pods back

---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,50 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// CocoaLumberjack
+#define COCOAPODS_POD_AVAILABLE_CocoaLumberjack
+#define COCOAPODS_VERSION_MAJOR_CocoaLumberjack 1
+#define COCOAPODS_VERSION_MINOR_CocoaLumberjack 6
+#define COCOAPODS_VERSION_PATCH_CocoaLumberjack 2
+
+// PaymentKit
+#define COCOAPODS_POD_AVAILABLE_PaymentKit
+#define COCOAPODS_VERSION_MAJOR_PaymentKit 1
+#define COCOAPODS_VERSION_MINOR_PaymentKit 1
+#define COCOAPODS_VERSION_PATCH_PaymentKit 1
+
+// Stripe
+#define COCOAPODS_POD_AVAILABLE_Stripe
+#define COCOAPODS_VERSION_MAJOR_Stripe 3
+#define COCOAPODS_VERSION_MINOR_Stripe 1
+#define COCOAPODS_VERSION_PATCH_Stripe 0
+
+// Stripe/Core
+#define COCOAPODS_POD_AVAILABLE_Stripe_Core
+#define COCOAPODS_VERSION_MAJOR_Stripe_Core 3
+#define COCOAPODS_VERSION_MINOR_Stripe_Core 1
+#define COCOAPODS_VERSION_PATCH_Stripe_Core 0
+
+// THContactPicker
+#define COCOAPODS_POD_AVAILABLE_THContactPicker
+#define COCOAPODS_VERSION_MAJOR_THContactPicker 0
+#define COCOAPODS_VERSION_MINOR_THContactPicker 0
+#define COCOAPODS_VERSION_PATCH_THContactPicker 3
+
+// VLBCameraView
+#define COCOAPODS_POD_AVAILABLE_VLBCameraView
+#define COCOAPODS_VERSION_MAJOR_VLBCameraView 2
+#define COCOAPODS_VERSION_MINOR_VLBCameraView 1
+#define COCOAPODS_VERSION_PATCH_VLBCameraView 0
+
+// VLBFoundation
+#define COCOAPODS_POD_AVAILABLE_VLBFoundation
+#define COCOAPODS_VERSION_MAJOR_VLBFoundation 1
+#define COCOAPODS_VERSION_MINOR_VLBFoundation 0
+#define COCOAPODS_VERSION_PATCH_VLBFoundation 0
+
