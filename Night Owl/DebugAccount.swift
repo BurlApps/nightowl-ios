ef554082f31350bf13b89e46f12395293a217ee9
Brian Vallelunga <vallelungabrian@gmail.com>
2015-04-01 12:51:29 -0700

    fixed debug accounts

---
+++ Night Owl/DebugAccount.swift
@@ -0,0 +1,13 @@
+//
+//  DebugAccounts.swift
+//  Night Owl
+//
+//  Created by Brian Vallelunga on 4/1/15.
+//  Copyright (c) 2015 Brian Vallelunga. All rights reserved.
+//
+
+import Cocoa
+
+class DebugAccounts: NSObject {
+
+}
