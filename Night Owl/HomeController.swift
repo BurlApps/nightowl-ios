e0313b337caa09d61f5b995e39f1b314a9edbc38
Brian Vallelunga <vallelungabrian@gmail.com>
2015-05-15 01:22:55 -0700

    made new user more effecient

---
+++ Night Owl/HomeController.swift
@@ -0,0 +1,13 @@
+//
+//  HomeController.swift
+//  Night Owl
+//
+//  Created by Brian Vallelunga on 5/14/15.
+//  Copyright (c) 2015 Brian Vallelunga. All rights reserved.
+//
+
+import Cocoa
+
+class HomeController: UIViewController {
+
+}
